df6e288e60fcff32c6bbabfef363a8b91739acf7
Schambes <Schambes@Schambes-Laptop.Speedport_W_921V_1_43_000>
2017-08-29 17:28:56 +0200

    Utils Update

--- src/de/Schambes/Hangman/Utils.java
+++ src/de/Schambes/Hangman/Utils.java
@@ -1,15 +1,55 @@
 package de.Schambes.Hangman;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 public class Utils {
 	
-	private static String[] words = {
-			"hello",
-			"test",
-	};
-			
+	private ArrayList<String> words = new ArrayList<>();
+	
+	private Utils() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	private void update() throws IOException {
+		//Serverconnection
+		ArrayList<String> words = new ArrayList<>();
+		words.add("Server");
+		//
+		this.words = words;
+		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(new File("res.jps")));
+		out.writeObject(this.words);
+		out.close();
+	}
+		
 	
 	public static String getWord() {
-		return words[(int) (Math.random()*(words.length - 1))];
+		Utils u = new Utils();
+		try {
+			u.update();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		try {
+			u.load();
+		} catch (Exception e) {
+			// TODO: handle exception
+			return "Sorry";
+		}
+		int r = (int) (Math.random()*100)+1;
+		return u.words.get(r%u.words.size());
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void load() throws IOException, ClassNotFoundException {
+		ObjectInputStream in = new ObjectInputStream(new FileInputStream(new File("res.jps")));
+		this.words = (ArrayList<String>) in.readObject();
+		in.close();
 	}
 	
 }
